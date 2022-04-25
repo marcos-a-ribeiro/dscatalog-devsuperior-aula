bd9c2946854aa31bd81a4968a9e92dfd28d994cb
Marcos Antonio Ribeiro <maribeiro9600@gmail.com>
2022-04-24 21:17:58 -0300

    Custom validation response

--- backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -26,7 +26,7 @@ public class ProductDTO implements Serializable {
 	@NotBlank(message = "Campo requerido")
 	private String description;
 
-	@Positive(message = "Preço deve ser um valor positovo")
+	@Positive(message = "Preço deve ser um valor positivo")
 	private Double price;
 	private String imgUrl;
 
